abbaea79ad649b35d30908581e6d22721a3650b9
Shrenik Mehar <111577600+ShrenikMehar@users.noreply.github.com>
2023-01-20 21:34:32 +0530

    Added via Upload

--- AdviceTeller/urls.py
+++ AdviceTeller/urls.py
@@ -20,6 +20,4 @@ from AdviceTeller import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.index),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
